e495fac5e3ac9cff755b1488701ffe594f98da77
ShaKristen2ybok <ufxthl560@163.com>
2021-01-22 22:05:15 +0800

    On peut lire un csv, maintenant à savori comment on stocke l'information/comment on y accède

--- TP1-KNN/src/main/java/main.java
+++ TP1-KNN/src/main/java/main.java
@@ -1,8 +1,22 @@
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Created by Alexandre on 19/10/2016.
  */
+
+
 public class main {
     public static void main(String[] args){
-        System.out.println("HELLO WORLD");
+        try {
+            CSVReader reader = new CSVReader(new FileReader("data/TP1/iris.data"));
+            List myEntries = reader.readAll();
+            System.out.println("Chargé !");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
