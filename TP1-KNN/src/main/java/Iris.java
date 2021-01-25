704f1290ff771a189e2b51463ef77fd035587db4
ShaKristen2ybok <ufxthl560@163.com>
2021-01-25 21:25:05 +0800

    added iris class

---
+++ TP1-KNN/src/main/java/Iris.java
@@ -0,0 +1,20 @@
+import java.util.Collections;
+
+/**
+ * Created by Alexandre on 19/10/2016.
+ */
+public class Iris {
+    float sepalLength;
+    float sepalWidth;
+    float petalLength;
+    float petalWidth;
+    String type;
+
+    public Iris(float sL, float  sW, float  pL, float  pW, String type){
+        this.sepalLength = sL;
+        this.sepalWidth = sW;
+        this.petalLength = pL;
+        this.petalWidth = pW;
+        this.type = type;
+    }
+}
